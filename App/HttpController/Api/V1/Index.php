9a384923c19eec0ece4165ec58b51ae660aa3380
李沈阳 <lsy_php@163.com>
2019-10-13 18:47:22 +0800

    23131

--- App/HttpController/Api/V1/Index.php
+++ App/HttpController/Api/V1/Index.php
@@ -12,13 +12,14 @@ namespace App\HttpController\Api\V1;
 use App\HttpController\Base;
 use App\Utility\Pool\RedisObject;
 use App\Utility\Pool\RedisPool;
+use EasySwoole\Redis\Config\RedisConfig;
 use EasySwoole\Redis\Redis;
 
 class Index extends Base
 {
     public function test()
     {
-         $redisConfig = new \EasySwoole\Mysqli\Config([
+         $redisConfig = new RedisConfig([
              'host' => '127.0.0.1',
              'port' => 6379,
          ]);
